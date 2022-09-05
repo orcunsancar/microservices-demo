c63014aaa2f417491f5e463346fdfcc4dc073447
Orcun Sancar <eorcunsancar@gmail.com>
2022-09-05 11:51:29 +0300

    MCD20: implemented jasypt encryption

---
+++ playground/src/main/java/jasypt/TestJasypt.java
@@ -0,0 +1,19 @@
+package jasypt;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.iv.RandomIvGenerator;
+
+public class TestJasypt {
+
+    public static void main(String[] args) {
+
+        StandardPBEStringEncryptor standardPBEStringEncryptor = new StandardPBEStringEncryptor();
+        standardPBEStringEncryptor.setPassword("Demo_Pwd!2020");
+        standardPBEStringEncryptor.setAlgorithm("PBEWithHMACSHA512AndAES_256");
+        standardPBEStringEncryptor.setIvGenerator(new RandomIvGenerator());
+        String result = standardPBEStringEncryptor.encrypt("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");
+        System.out.println(result);
+        System.out.println(standardPBEStringEncryptor.decrypt(result));
+
+    }
+}
